321396eec719c47896c7e987f799c691b54506d1
ThallesSorrilha <thallessorrilha@gmail.com>
2022-06-20 11:02:21 -0300

    n2006v2

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 
 // Conexão com o banco de dados
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/teste') //{ useMongoClient: true })
+mongoose.connect('mongodb://localhost:27017/projeto-fullstack-web',{ useMongoClient: true })
     .then(() => {
         console.log("++ Conexão realizada com sucesso!")
     })
